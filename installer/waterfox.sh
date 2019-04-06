35ed22f80b7a9e634779120823fb461706039e9c
2nd-junkey <broken.phantsm@gmail.com>
2019-04-06 18:57:44 +0900

    add check curl

--- installer/waterfox.sh
+++ installer/waterfox.sh
@@ -1,7 +1,13 @@
-sudo apt install -y curl
-echo "deb https://dl.bintray.com/hawkeye116477/waterfox-deb release main" | sudo tee -a /etc/apt/sources.list &&
-    curl https://bintray.com/user/downloadSubjectPublicKey?username=hawkeye116477 | sudo apt-key add -
-sudo apt-get update && sudo apt-get install -y waterfox
+type curl >/dev/null 2&>1
+if [ "$?" -ne "0" ]
+then
+	sudo apt install -y curl
+fi
+
+echo "deb https://dl.bintray.com/hawkeye116477/waterfox-deb release main" | sudo tee -a /etc/apt/sources.list \
+	&& curl https://bintray.com/user/downloadSubjectPublicKey?username=hawkeye116477 | sudo apt-key add -
+sudo apt update \
+	&& sudo apt install -y waterfox
 
 # isntall late tab groups
 # https://github.com/JustOff/ca-archive/releases
